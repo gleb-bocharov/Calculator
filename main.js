303007d4e2ca45a13136f649b9061a7d71352ffc
gleb-bocharov <gleb4151@gmail.com>
2017-06-03 16:26:29 +0300

    added button types

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-var 
+function loaded(){
+	var buttons = document.querySelectorAll("div.keysPanel > div.keysRow > div");
+	buttons.forEach(function(button){
+		button.addEventListener("click", onButtonClick);
+	})
+	var clearButton = document.querySelector("div.topRow > div.clearButton");
+	clearButton.addEventListener("click", onButtonClick);
+	
+} 
+
+function onButtonClick(e){
+	var type = e.target.attributes["button-type"].value;
+	var value = e.target.attributes["button-value"].value;
+	console.log(type);
+	console.log(value);
+	
+}
